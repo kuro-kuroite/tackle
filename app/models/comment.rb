cde86a2ec1332e40011efac13853d1663bb43847
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-03-27 17:46:12 +0900

    improve validate bacause of form error duplicate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :task
 
-  validates :content, presence: true, length: { in: 1..256 }
+  validates :content, length: { in: 1..256 }
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   has_many :comments, dependent: :destroy
 
-  validates :name, presence: true, length: { in: 1..64 }
+  validates :name, length: { in: 1..64 }
   validates :deadline, presence: true
 end
