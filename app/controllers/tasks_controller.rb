8f2e6634f900de1c919143d5b2c46d062a4b291e
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-03-27 18:17:37 +0900

    add latest comment in each task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,7 +22,6 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
     @task = Task.new
-    # @comment = Comment.find_by(task_id: params[:id]) # TODO: index で最新のコメントのみを表示したい
   end
 
   def new
