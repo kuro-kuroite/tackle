1eead6c1348e848fbc45dc8187c71b16884e8e49
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-03-27 17:58:30 +0900

    Task#indexに新規form を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,6 +21,7 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.all
+    @task = Task.new
     # @comment = Comment.find_by(task_id: params[:id]) # TODO: index で最新のコメントのみを表示したい
   end
 
