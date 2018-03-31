5a6ac36326ee86d3f22f587af28d001ad5a7aba4
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-03-31 22:18:57 +0900

    change utc locale in database and jst locale in view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,11 @@ class TasksController < ApplicationController
   # TODO: CRUD の開発手順を考察、C、V
   def create
     @task = Task.new(task_params)
+    # FIXME: request から地域情報を取得して、create内で JST -> UTC変換をできるようにする
+    tz = TZInfo::Country.get("JP")
+    @task.deadline = @task.deadline.in_time_zone(tz.zone_names[0])
+    # HACK: 9 の部分を地域情報から動的に変更できるようにする
+    @task.deadline -= 9.hours
     if @task.save
       redirect_to @task
     else
